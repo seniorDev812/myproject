d533278e52caebede98ad2bdfa2a331d0af7defe
seniorDev812 <cassianrourke@gmail.com>
2025-07-19 03:01:46 -0700

    add letter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-
 import './App.css'
 
 function App() {
@@ -26,7 +25,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more and more
       </p>
     </>
   )
